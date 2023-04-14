6085aea957eb491f2d65773fe66950b79f705a03
devartes <anacavalcante731@gmail.com>
2023-04-14 16:19:14 -0300

    feat(twind.config.ts): add classes for header

--- twind.config.ts
+++ twind.config.ts
@@ -1,7 +1,24 @@
 /** @type {import('$fresh/plugins/twind').Options} */
 export default {
+  plugins: {
+    "l-container": "max-w-[1200px] sm:w-[calc(100% - 24px)]",
+    "flex-center-between": "flex items-center justify-between",
+    "flex-center-end": "flex items-center justify-end",
+    "flex-center-center" : "flex items-center justify-center",
+    "is-sticky": {
+      filter:
+        "invert(100%) sepia(100%) saturate(0%) hue-rotate(0deg) brightness(100%) contrast(100%)",
+    },
+    "letter-spacing": { letterSpacing: "1px" },
+    "color-green": { color: "#005046" },
+    "color-green-lemon": {
+      filter:
+        "invert(84%) sepia(20%) saturate(530%) hue-rotate(112deg) brightness(200%) contrast(95%)",
+    },
+  },
   theme: {
     extend: {
+      backgroundImage: {},
       screens: {
         sm: "640px",
         md: "768px",
