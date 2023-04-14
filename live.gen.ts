f204be55c17a4ea2c4be2ebfece06197b340ca27
devartes <anacavalcante731@gmail.com>
2023-04-14 16:29:58 -0300

    feat: add Header and Main to the main structure

--- live.gen.ts
+++ live.gen.ts
@@ -7,8 +7,12 @@ import { DecoManifest } from "$live/types.ts";
 
 import * as $0 from "./functions/LoadGitHubRaw.ts";
 import * as $$$$0 from "./routes/_app.tsx";
-import * as $$$$$$$$0 from "./sections/Head.tsx";
-import * as $$$$$$$$1 from "./sections/GetStarted.tsx";
+import * as $$$$$0 from "./islands/ButtonHamburger.tsx";
+import * as $$$$$1 from "./islands/Header.tsx";
+import * as $$$$$2 from "./islands/Navlink.tsx";
+import * as $$$$$3 from "./islands/ScrollClassRemover.tsx";
+import * as $$$$$$$$0 from "./sections/GetStarted.tsx";
+import * as $$$$$$$$1 from "./sections/Head.tsx";
 import * as $live_middleware from "$live/routes/_middleware.ts";
 import * as $live_workbench from "$live/routes/live/workbench.ts";
 import * as $live_editorData from "$live/routes/live/editorData.ts";
@@ -46,9 +50,15 @@ const manifest: DecoManifest = {
     "./routes/live/previews/[...block].tsx": $live_previews,
     "./routes/[...catchall].tsx": $live_catchall,
   },
+  "islands": {
+    "./islands/ButtonHamburger.tsx": $$$$$0,
+    "./islands/Header.tsx": $$$$$1,
+    "./islands/Navlink.tsx": $$$$$2,
+    "./islands/ScrollClassRemover.tsx": $$$$$3,
+  },
   "sections": {
-    "deco-sites/kaszek/sections/Head.tsx": $$$$$$$$0,
-    "deco-sites/kaszek/sections/GetStarted.tsx": $$$$$$$$1,
+    "deco-sites/kaszek/sections/GetStarted.tsx": $$$$$$$$0,
+    "deco-sites/kaszek/sections/Head.tsx": $$$$$$$$1,
     "$live/sections/PageInclude.tsx": i1$$$0,
   },
   "handlers": {
@@ -73,7 +83,6 @@ const manifest: DecoManifest = {
     "$live/flags/audience.ts": i1$$$$$0,
     "$live/flags/everyone.ts": i1$$$$$1,
   },
-  "islands": {},
   "config": config,
   "baseUrl": import.meta.url,
 };
