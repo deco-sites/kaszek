dee3106bfe8f3926cb1d39c32f469f3897d17503
devartes <anacavalcante731@gmail.com>
2023-04-17 17:17:56 -0300

    refactor(AboutKaszek.tsx): adjust positioning and add max-width to parent div

--- components/Main/AboutKaszek.tsx
+++ components/Main/AboutKaszek.tsx
@@ -5,18 +5,18 @@ export default function AboutKaszek() {
   return (
     <section class="w-full flex items-center flex-col bg-[#ebf0ef] px-[12px] md:px-0">
       <div class="w-full l-container grid grid-rows-1 md:grid-cols-2 gap-4">
-        <div class="h-full relative">
+        <div class="h-full relative max-w-[509.99px]">
           <object data={asset(`/imgs/america.svg`)}
             width="100%"
             class="max-h-[620.99px] object-cover pointer-events-none"
           />
           <CityPlate name="São Paulo" bottom="32%" left="74%" />
-          <CityPlate name="Mexico DF" bottom="75%" left="23.5%" />
+          <CityPlate name="Mexico DF" bottom="75%" left="18.5%" />
           <CityPlate name="Buenos Aires" bottom="21%" left="66%" />
           <CityPlate name="Curitiba" bottom="28%" left="73%" />
           <CityPlate name="Medellín" bottom="62%" left="49%" />
           <CityPlate name="Santiago" bottom="21%" left="54%" />
-          <CityPlate name="Guadalajara" bottom="77%" left="20%" />
+          <CityPlate name="Guadalajara" bottom="77%" left="15%" />
           <CityPlate name="Mendoza" bottom="22.5%" left="58.5%" />
           <CityPlate name="Miami" bottom="83.5%" left="40.5%" />
           <CityPlate name="Montevideo" bottom="21.2%" left="67.5%" />
