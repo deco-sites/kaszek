1b2cd2f53ea3244fe232ef2046b292945905ac5b
devartes <anacavalcante731@gmail.com>
2023-04-20 10:32:31 -0300

    refactor(AboutKaszek.tsx): replaced object tag with Image component

--- components/Main/AboutKaszek.tsx
+++ components/Main/AboutKaszek.tsx
@@ -1,9 +1,11 @@
-import { asset } from "$fresh/runtime.ts";
+import type { Image as LiveImage } from "deco-sites/std/components/types.ts";
+import Image from "deco-sites/std/components/Image.tsx";
 import CityPlate from "deco-sites/start/components/Main/CityPlate.tsx";
 import { useState } from "preact/hooks";
 
 export interface Props {
-  city: Cities[];
+  img: LiveImage;
+  city?: Cities[];
   title: string;
   first_description: string;
   second_description?: string;
@@ -22,19 +24,14 @@ export default function AboutKaszek(props: Props) {
     <section class="w-full flex items-center flex-col bg-[#ebf0ef] px-[12px] md:px-0">
       <div class="w-full l-container grid grid-rows-1 md:grid-cols-2 gap-4">
         <div class="h-full relative max-w-[509.99px]">
-          <object
-            data={asset(`/imgs/america.svg`)}
-            width="100%"
-            class="max-h-[620.99px] object-cover pointer-events-none"
-          />
+           <Image class="max-h-[620.99px] object-cover pointer-events-none" src={props.img} alt={"background"} width={509.99} height={620.99}/>
           {locations.map((city, index) => (
             <CityPlate
             key={index}
             name={city.city ? city.city : ""} 
             bottom={city.bottom}
             left={city.left}
-          />
-          
+          />        
           ))}      
         </div>
         <div class="w-full md:max-w-[384px] flex-center-center flex-col mx-auto">
