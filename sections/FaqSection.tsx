cf703049457b47b224e111e1b0047f371ee31730
devartes <anacavalcante731@gmail.com>
2023-04-21 13:26:53 -0300

    chore(sections): remove comments from FaqSection

--- sections/FaqSection.tsx
+++ sections/FaqSection.tsx
@@ -1,11 +1,2 @@
-// import { Props } from "deco-sites/start/components/Main/FaqSection.tsx";
-
 export { default } from "deco-sites/start/components/Main/FaqSection.tsx";
 export type { Faq, Props } from "../components/Main/FaqSection.tsx";
-
-
-// function FaqSection(props: Props) {
-//     return <FaqSection {...props} />;
-//   }
-  
-//   export default FaqSection;
