94a25c9d2cf4d54e2516a10538adea716196ceab
devartes <anacavalcante731@gmail.com>
2023-04-23 15:50:57 -0300

    refactor: replace className with class

--- components/Header/ButtonHamburger.tsx
+++ components/Header/ButtonHamburger.tsx
@@ -11,13 +11,15 @@ export default function ButtonHamburger(props: ButtonHamburgerProps) {
   return (
     <button
       onClick={toggleMenu}
-      className={`block md:hidden z-10 relative focus:outline-none focus:border-none ${showMenu ? 'w-[48px] h-[48px]' : 'w-8 h-[13.99px] right-[12px]'}`}
-      style={{ WebkitTapHighlightColor: 'transparent' }}
+      class={`block md:hidden z-10 relative focus:outline-none focus:border-none ${
+        showMenu ? "w-[48px] h-[48px]" : "w-8 h-[13.99px] right-[12px]"
+      }`}
+      style={{ WebkitTapHighlightColor: "transparent" }}
       aria-label="menu button"
     >
       <object
         data={asset(`/icon-x.svg`)}
-        className={`block absolute bg-white top-0 rounded-full right-[16px]`}
+        class={`block absolute bg-white top-0 rounded-full right-[16px]`}
         style={{
           width: "48px",
           height: "48px",
@@ -26,18 +28,18 @@ export default function ButtonHamburger(props: ButtonHamburgerProps) {
         }}
       />
       <span
-        className={`${
+        class={`${
           showMenu ? "" : "block absolute"
-        } top-[1px] w-full h-[3px] rounded-md ${window.scrollY > 0 ? 'bg-[#005046]' : 'bg-white'} ${
-          showMenu ? "opacity-0" : ""
-        }`}
+        } top-[1px] w-full h-[3px] rounded-md ${
+          window.scrollY > 0 ? "bg-[#005046]" : "bg-white"
+        } ${showMenu ? "opacity-0" : ""}`}
       />
       <span
-        className={`${
+        class={`${
           showMenu ? "" : " block absolute"
-        } top-[80%] w-full h-[3px] rounded-md ${window.scrollY > 0 ? 'bg-[#005046]' : 'bg-white'} ${
-          showMenu ? "-rotate-45" : ""
-        }`}
+        } top-[80%] w-full h-[3px] rounded-md ${
+          window.scrollY > 0 ? "bg-[#005046]" : "bg-white"
+        } ${showMenu ? "-rotate-45" : ""}`}
       />
     </button>
   );
