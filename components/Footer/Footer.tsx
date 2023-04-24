1cd4a96ad3b79fc2f1201656459a94e89d93c3d3
devartes <anacavalcante731@gmail.com>
2023-04-24 10:50:38 -0300

    refactor(Footer.tsx): remove unnecessary white spaces in code

--- components/Footer/Footer.tsx
+++ components/Footer/Footer.tsx
@@ -9,7 +9,6 @@ export type SocialMedia = {
   social_media_link?: string;
 };
 
-
 export type Contents = {
   first_text?: string;
   email_name?: string;
@@ -20,11 +19,7 @@ export type Contents = {
 };
 
 export default function Footer(props: Props) {
-  const [socialMediaLinks] = useState(
-    Array.isArray(props.contents[0].social_media) ? props.contents[0].social_media : []
-  );
-  
-  
+  const [socialMediaLinks] = useState(Array.isArray(props.contents[0].social_media) ? props.contents[0].social_media : []);
   const currentYear = new Date().getFullYear();
 
   return (
@@ -33,10 +28,7 @@ export default function Footer(props: Props) {
         <div class="grid lg:grid-cols-3 lg:gap-[24px] md:gap-y-0 gap-x-[24px] gap-y-[60px] lg:mb-[150px] mb-[120px] md:grid-cols-2 grid-cols-1">
           <div>
             <p class="text-[16px] Maax-Regular-Font">{props.contents[0]?.first_text}</p>
-            <a
-              href={props.contents[0]?.email_link}
-              class="color-green uppercase text-[14px] tracking-[1px] Maax-Bold-Font"
-            >
+            <a href={props.contents[0]?.email_link} class="color-green uppercase text-[14px] tracking-[1px] Maax-Bold-Font">
               {props.contents[0]?.email_name}
             </a>
           </div>
@@ -47,11 +39,7 @@ export default function Footer(props: Props) {
             <ul class="lg:flex-center-end flex-wrap flex items-center justify-start lg:justify-end">
             {socialMediaLinks.map((media: SocialMedia, index: number) => (
               <li key={index}>
-                <a
-                  target="_blank"
-                  rel="noopener"
-                  href={media.social_media_link}
-                  class="arrow-icon color-green uppercase text-[14px] tracking-[1px] mr-[30px] Maax-Bold-Font">
+                <a target="_blank" rel="noopener" href={media.social_media_link} class="arrow-icon color-green uppercase text-[14px] tracking-[1px] mr-[30px] Maax-Bold-Font">
                   {media.social_media_name}
                 </a>
               </li>
