46427eb2a249d5fd6f87e14531a23957e1737236
devartes <anacavalcante731@gmail.com>
2023-05-28 14:01:22 -0300

    refactor(InvestmentStages.tsx): replace className with class

--- components/ui/Ethos/InvestmentStages.tsx
+++ components/ui/Ethos/InvestmentStages.tsx
@@ -15,10 +15,10 @@ export default function InvestmentStages(props: Props) {
   );
 
   return (
-    <div className="mx-auto xl:max-w-[1200px] xl:px-0 px-[15px] w-full lg:mt-[120px] md:mt-[75px] mt-[50px]">
-      <div className="w-full grid md:gap-[24px] gap-[75px] grid-cols-1 md:grid-cols-five">
+    <div class="mx-auto xl:max-w-[1200px] xl:px-0 px-[15px] w-full lg:mt-[120px] md:mt-[75px] mt-[50px]">
+      <div class="w-full grid md:gap-[24px] gap-[75px] grid-cols-1 md:grid-cols-five">
         {investmentList.map((text, index) => (
-          <div key={index} className="text-center">
+          <div key={index} class="text-center">
             {text.title && (
               <p class="text-[#005046] Maax-Bold-Font md:mb-[32px] mb-[16px] text-[24px] leading-[30px] tracking-[-0.5px]">
                 {text.title}
