8e28d9c3b9e96e4cecd9094ec3bd414cbc5d842c
devartes <anacavalcante731@gmail.com>
2023-06-17 14:16:37 -0300

    refactor(Weather.tsx): added the new sunny-intervals icon, add custom icon size, flex, and gap options

--- components/ui/Main/Weather.tsx
+++ components/ui/Main/Weather.tsx
@@ -4,18 +4,30 @@ import { useEffect, useState } from "preact/hooks";
 export type Props = {
   latitude?: number;
   longitude?: number;
+  iconSize?: number;
+  isMobileFlex?: "flex-row" | "flex-col";
+  gapTemperatureAndIcon?: "gap-0" | "gap-[8px]";
 };
 
-async function fetchTemperature({ latitude, longitude }: Props) {
-  const url = `https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&current_weather=true`;
+const DEFAULT_WIDTH = 23.99;
+
+async function fetchTemperature({ latitude, longitude, iconSize }: Props) {
+  const url =
+    `https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&current_weather=true`;
   const response = await fetch(url);
   const json = await response.json();
   const { current_weather } = json;
   const { temperature, weathercode, is_day } = current_weather;
-  return { temperature, weathercode, is_day };
+  return { temperature, weathercode, is_day, iconSize };
 }
 
-export default function Weather({ latitude, longitude }: Props) {
+export default function Weather({
+  latitude,
+  longitude,
+  iconSize,
+  isMobileFlex,
+  gapTemperatureAndIcon,
+}: Props) {
   const [temperature, setTemperature] = useState<number | null>(null);
   const [icon, setIcon] = useState<string>("rain");
   const [isLoading, setIsLoading] = useState<boolean>(true);
@@ -25,50 +37,70 @@ export default function Weather({ latitude, longitude }: Props) {
       const { temperature, weathercode } = await fetchTemperature({
         latitude,
         longitude,
+        iconSize,
       });
       setTemperature(temperature);
-      if (temperature <= 13 && temperature >= 0) {
+      if (temperature >= 0 && temperature <= 13) {
         setIcon("sun");
+      } else if (temperature >= 14 && temperature <= 22) {
+        setIcon("sunny-intervals");
+      } else if (weathercode >= 23 && weathercode <= 48) {
+        setIcon("cloud");
       } else {
-        setIcon(weathercode > 45 ? "rain" : "cloud");
+        setIcon("rain");
       }
       setIsLoading(false);
     }
     getTemperature();
-  }, [latitude, longitude]);
+  }, [latitude, longitude, iconSize]);
+
+  const defaultIconSize = iconSize || DEFAULT_WIDTH;
+
+  const flexClasses = `flex ${
+    isMobileFlex === "flex-row" ? "" : "md:flex-row"
+  } ${isMobileFlex || ""} items-center ${
+    gapTemperatureAndIcon ? `${gapTemperatureAndIcon}` : ""
+  }`;
 
   return (
-    <div class="flex items-center">
-      {!isLoading && icon === "cloud" ? (
+    <div class={flexClasses}>
+      {!isLoading && icon === "cloud" && (
         <object
           data={asset(`/icon-cloud.svg`)}
-          width="23.99"
-          height="23.99"
-          class="mr-[8px]"
+          width={defaultIconSize.toString()}
+          height={defaultIconSize.toString()}
           aria-label="icon cloud"
-        ></object>
-      ) : !isLoading && icon === "rain" ? (
+        />
+      )}
+      {!isLoading && icon === "rain" && (
         <object
           data={asset(`/icon-rain.svg`)}
-          width="23.99"
-          height="23.99"
-          class="mr-[8px]"
+          width={defaultIconSize.toString()}
+          height={defaultIconSize.toString()}
           aria-label="icon rain"
-        ></object>
-      ) : !isLoading && icon === "sun" ? (
+        />
+      )}
+      {!isLoading && icon === "sun" && (
         <object
           data={asset(`/icon-sun.svg`)}
-          width="23.99"
-          height="23.99"
-          class="mr-[8px]"
+          width={defaultIconSize.toString()}
+          height={defaultIconSize.toString()}
           aria-label="icon sun"
-        ></object>
-      ) : null}
-      {!isLoading && temperature !== null ? (
+        />
+      )}
+      {!isLoading && icon === "sunny-intervals" && (
+        <object
+          data={asset(`/icon-sunny-intervals.svg`)}
+          width={defaultIconSize.toString()}
+          height={defaultIconSize.toString()}
+          aria-label="icon sunny intervals"
+        />
+      )}
+      {!isLoading && temperature !== null && (
         <span class="text-[#83ff97] text-[12px] z-2 md:text-[14px]">
-          {Math.round(temperature)} °C
+          {Math.round(temperature)}°C
         </span>
-      ) : null}
+      )}
     </div>
   );
 }
