6070867636737679a454d3e4c201734eb47425bd
devartes <anacavalcante731@gmail.com>
2023-07-06 19:22:27 -0300

    refactor(Footer.tsx): improving the UI, modifying some names and added a formatted textarea where needed

--- components/ui/Footer/Footer.tsx
+++ components/ui/Footer/Footer.tsx
@@ -5,45 +5,47 @@ export interface Props {
 }
 
 export type SocialMedia = {
-  social_media_name?: string;
-  social_media_link?: string;
+  socialMediaName?: string;
+  socialMediaLink?: string;
 };
 
-export type Contents = {
-  first_text?: string;
-  email_name?: string;
-  email_link?: string;
-  second_text?: string;
-  social_media?: SocialMedia[];
+export interface Contents {
+  /** @format textarea */
+  firstText?: string;
+  emailName?: string;
+  emailLink?: string;
+  /** @format textarea */
+  secondText?: string;
+  socialMedia?: SocialMedia[];
   copyright?: string;
-};
+}
 
 export default function Footer(props: Props) {
   const [socialMediaLinks] = useState(
-    Array.isArray(props.contents[0].social_media)
-      ? props.contents[0].social_media
+    Array.isArray(props.contents[0].socialMedia)
+      ? props.contents[0].socialMedia
       : [],
   );
   const currentYear = new Date().getFullYear();
 
   return (
-    <footer class="bg-[#ebf0ef]  bottom-0 relative pt-[100px] pb-[50px] border-b-4 border-solid border-[#83ff97]">
+    <footer class="bg-[#ebf0ef] bottom-0 relative pt-[100px] pb-[50px] border-b-4 border-solid border-[#83ff97]">
       <div class="w-full max-w-[1200px] mx-auto xl:px-0 px-[12px]">
         <div class="grid lg:grid-cols-3 grid-rows-1 lg:gap-[24px] lg:gap-y-0 gap-x-[24px] gap-y-[60px] lg:mb-[150px] mb-[120px] md:grid-cols-2 grid-cols-1">
           <div>
             <p class="text-[16px] Maax-Regular-Font font-normal break-words hyphens-auto">
-              {props.contents[0]?.first_text}
+              {props.contents[0]?.firstText}
             </p>
             <a
-              href={props.contents[0]?.email_link}
+              href={props.contents[0]?.emailLink}
               class="text-[#005046] uppercase text-[14px] tracking-[1px] Maax-Bold-Font font-bold leading-[16px]"
             >
-              {props.contents[0]?.email_name}
+              {props.contents[0]?.emailName}
             </a>
           </div>
           <div>
             <p class="text-[16px] Maax-Regular-Font font-normal break-words hyphens-auto">
-              {props.contents[0]?.second_text}
+              {props.contents[0]?.secondText}
             </p>
           </div>
           <div>
@@ -53,10 +55,10 @@ export default function Footer(props: Props) {
                   <a
                     target="_blank"
                     rel="noopener"
-                    href={media.social_media_link}
+                    href={media.socialMediaLink}
                     class="arrow-icon text-[#005046] uppercase text-[14px] tracking-[1px] Maax-Bold-Font font-bold"
                   >
-                    {media.social_media_name}
+                    {media.socialMediaName}
                   </a>
                 </li>
               ))}
